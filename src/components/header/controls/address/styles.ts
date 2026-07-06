13a43c247125c06b185e07d00a62d1ed2d44b42c
Grosu Igor <igrosu@griddynamics.com>
2026-07-06 15:36:10 +0300

    refactor(header): splitted controls

--- src/components/header/controls/address/styles.ts
+++ src/components/header/controls/address/styles.ts
@@ -1,28 +1,21 @@
 import type { StylesGroup } from "@/lib/theme";
 
 export const styles: StylesGroup = {
-  root: {
-    alignItems: "center",
-  },
-  clusterizationText: {
-    color: "text.secondary",
-    whiteSpace: "nowrap",
-  },
-  settingsInput: (theme) => ({
+  input: (theme) => ({
     minWidth: 400,
     "& .MuiInputBase-root": {
       borderRadius: theme.radii.pill,
     },
   }),
-  settingsButton: { pointerEvents: "auto", mr: 0 },
-  settingsButtonIcon: {
+  inputIcon: { pointerEvents: "auto", mr: 0 },
+  buttonIcon: {
     color: "text.primary",
     fontSize: 18,
     "&:hover, &.Mui-focusVisible, &.Mui-focused": {
       color: "var(--mui-iconButton-hoverBg)",
     },
   },
-  settingsSearchIcon: {
+  searchIcon: {
     color: "text.secondary",
     fontSize: 18,
   },
