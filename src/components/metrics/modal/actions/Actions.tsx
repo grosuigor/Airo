6915b8d30443ca115fc1dcf7044f653f79646c29
Grosu Igor <igrosu@griddynamics.com>
2026-07-22 12:36:32 +0300

    refactor(metric modal actions): created a config

--- src/components/metrics/modal/actions/Actions.tsx
+++ src/components/metrics/modal/actions/Actions.tsx
@@ -1,20 +1,26 @@
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 
+import { buttonConfig } from "./config";
 import { useMetricsModalActions } from "./hooks";
 import { styles } from "./styles";
 
 export function Actions() {
-  const { unhideAll, hideAll } = useMetricsModalActions();
+  const callbacks = useMetricsModalActions();
 
   return (
     <Stack direction="row" sx={styles.root}>
-      <Button variant="outlined" color="primary" onClick={hideAll} sx={styles.actionButton}>
-        Hide all
-      </Button>
-      <Button variant="contained" color="primary" onClick={unhideAll} sx={styles.actionButton}>
-        Unhide all
-      </Button>
+      {buttonConfig.map((button) => (
+        <Button
+          key={button.action}
+          variant={button.variant}
+          color="primary"
+          onClick={callbacks[button.action]}
+          sx={styles.actionButton}
+        >
+          {button.label}
+        </Button>
+      ))}
     </Stack>
   );
 }
